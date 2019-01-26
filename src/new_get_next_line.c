7844877692feb5a15a3c4aff3642b84bd7d4048b
apavlyuc <apavlyuc@gmail.com>
2019-01-26 15:37:08 +0200

    refactor done; need tests

--- src/new_get_next_line.c
+++ src/new_get_next_line.c
@@ -24,43 +24,41 @@ static t_list		*get_current(t_list **list, const int fd)
 	return (temp);
 }
 
-static int			strcpy_to(char **dst, char *src)
+static t_ull		strcpy_to(char **dst, char *src)
 {
-	int				i;
+	t_ull			dst_len;
+	t_ull			i;
 
-	free((void *)*dst);
-	*dst = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
-	NULL_CHECK(dst);
-	i = -1;
-	while (*(src + ++i) != '\0' && *(src + i) != '\n')
+	dst_len = (ft_strlen(src) + 1) * sizeof(char);
+	*dst = (char *)ft_memalloc(dst_len);
+	//TODO: change macro in libft_defines.h
+	RETN_IF_NULL(((t_ull)(*dst)));
+	i = 0;
+	while (*(src + i) != '\0' && *(src + i) != '\n')
+	{
 		*(*dst + i) = *(src + i);
+		++i;
+	}
 	*(*dst + i) = '\0';
 	return (i);
 }
 
-static int			helper(char **line, t_list **current)
+static int			init_line(char **line, t_list **current)
 {
-	int				i;
-	char			*temp;
-	int				len;
+	t_ull			i;
+	char			*tmp;
+	t_ull			len;
 
-	if ((i = strcpy_to(line, (*current)->content)) == -1)
-		return (0);
-	len = (int)ft_strlen((*current)->content);
+	RETN_IF_NULL((i = strcpy_to(line, (*current)->content)));
+	len = ft_strlen((*current)->content);
+	tmp = 0;
 	if (i < len)
 	{
-		if (!(temp = ft_strsub((*current)->content, i + 1, len - i - 1)))
-			return (0);
-		free((void *)(*current)->content);
-		if (!((*current)->content = ft_strjoin("", temp)))
-			return (0);
-		free((void *)temp);
+		tmp = ft_strsub((*current)->content, i + 1, len - i - 1);
+		RETN_IF_NULL(((t_ull)tmp));
 	}
-	else
-	{
-		ft_strclr((*current)->content);
-		free((void *)(*current)->content);
-	}
+	ft_memdel((void **)(&(*current)->content));
+	(*current)->content = tmp;
 	return (1);
 }
 
@@ -74,17 +72,21 @@ int					new_get_next_line(const int fd, char **line)
 
 	ERR_CHECK(fd, line, read(fd, buf, 0));
 	NULL_CHECK((current = get_current(&list, fd)));
-	NULL_CHECK((*line = ft_strnew(0)));
 	while ((i = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[i] = 0;
 		NULL_CHECK((tmp = ft_strjoin(current->content, buf)));
-		free((void *)current->content);
-		current->content = ft_strdup(tmp);
-		free((void *)tmp);
+		ft_memdel((void **)(&current->content));
+		NULL_CHECK((current->content = ft_strdup(tmp)));
+		ft_memdel((void **)(&tmp));
 		BREAKER(ft_strchr(buf, '\n'));
 	}
-	END_CHECK(i, ft_strlen(current->content));
-	NULL_CHECK(helper(line, &current));
+	//END_CHECK(i, ft_strlen(current->content));
+	if (i < BUFF_SIZE && !ft_strlen(current->content))
+	{
+		free(current);
+		return (0);
+	}
+	NULL_CHECK(init_line(line, &current));
 	return (1);
 }
